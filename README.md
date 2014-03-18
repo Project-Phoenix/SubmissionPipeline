84f425f41b33206594874ac663aea5ad69791c06
Kilian Gärtner <Bhaals@gmx.de>
2014-03-18 11:31:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SubmissionPipeline
+
+Programm that validate, compile and test user submission out of the webservice. The webservice classpath is a plain horror and now we do this out of the box.
+
+Ugly, but neccessarry!
---
+++ src/main/java/de/phoenix/submissionpipeline/Core.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014 Project-Phoenix
+ * 
+ * This file is part of SubmissionPipeline.
+ * 
+ * SubmissionPipeline is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * SubmissionPipeline is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SubmissionPipeline.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.phoenix.submissionpipeline;
+
+public class Core {
+
+    public static void main(String[] args) {
+        System.out.println("Started!.");
+        
+        System.out.println("Ended!");
+    }
+
+}
